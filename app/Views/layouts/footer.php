96dfa212916dabe12c7546c6488cd39e343866b0
Dapittt <davidarifqisyahputra@gmail.com>
2023-05-25 12:47:28 +0700

    commit1

---
+++ app/Views/layouts/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer ">
+    <div class="container ">
+        <span class="text-muted ">Kopirek by DAPIT.</span>
+    </div>
+</footer>
---
+++ app/Views/layoutsV2/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Kopirek &copy; DAPIT </span>
+                    </div>
+                </div>
+            </footer>
